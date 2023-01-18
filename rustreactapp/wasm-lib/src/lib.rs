c6268e395447c9473f0e378213be631512ec60af
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-01-18 22:15:25 +0000

    Update lib.rs

--- rustreactapp/wasm-lib/src/lib.rs
+++ rustreactapp/wasm-lib/src/lib.rs
@@ -7,5 +7,7 @@ pub fn addnote(mut notelist: Vec<String>,element: String){
 fn main(){
     let note_list = Vec::new();
     let main = "he"; // this is an example ("he", will be whatever the user puts in as a note)
+    //so you will get the note feild and then pass in a value to this function
+    //than you can do a for loop through "notelist"
     addnote(note_list,main.to_string());
   }
