e656b55c4ffcf2b64fbfe01818bcd65ab2afe0f4
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-01-18 22:13:16 +0000

    Update lib.rs

--- rustreactapp/wasm-lib/src/lib.rs
+++ rustreactapp/wasm-lib/src/lib.rs
@@ -4,7 +4,7 @@ pub fn addnote(mut notelist: Vec<String>,element: String){
     notelist.push(element);
     println!("{:?}",notelist);
   }
-  fn main(){
+fn main(){
     let note_list = Vec::new();
     let main = "he"; // this is an example ("he", will be whatever the user puts in as a note)
     addnote(note_list,main.to_string());
