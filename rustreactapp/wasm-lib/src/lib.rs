f620a35236b94358a8a543f0e0ef6d3710b1cb17
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-01-19 19:23:24 +0000

    Update lib.rs

--- rustreactapp/wasm-lib/src/lib.rs
+++ rustreactapp/wasm-lib/src/lib.rs
@@ -1,24 +1,23 @@
 use wasm_bindgen::prelude::*;
-struct NoteApp;
-impl NoteApp{
-    #[wasm_bindgen]
-    pub fn addnote(mut notelist: Vec<String>,element: String){
-        notelist.push(element);
-        println!("{:?}",notelist);
-    }
-    #[wasm_bindgen]
-    pub fn delnote(mut notelist: Vec<String>,element:String){
-      let index = notelist.iter().position(element).unwrap();
-      notelist.remove(index);
-    }
-    #[wasm_bindgen]
-    pub fn get_values(mut notelist: Vec<String>) -> String {
-      return notelist;
-    }
+
+#[wasm_bindgen]
+pub fn addnote(mut notelist: Vec<String>,element: String){
+    notelist.push(element);
+    println!("{:?}",notelist);
+}
+#[wasm_bindgen]
+pub fn delnote(mut notelist: Vec<String>,element:String){
+    let index = notelist
+    .iter()
+    .position(|&r| r == element)
+    .unwrap();
+    notelist.remove(index);
+}
+#[wasm_bindgen]
+pub fn get_values(mut notelist: Vec<String>) -> Vec<String>{
+    notelist
 }
 fn main(){
-    let note_list = Vec::new();
-    //so you will get the note feild and then pass in a value to this function
-    //than you can do a for loop through "notelist"
-    // accessing the values of "notelist" by the "get_values" function
+    let note_list = Vec::<String>::new();
+    addnote(note_list,"je".to_string());
   }
