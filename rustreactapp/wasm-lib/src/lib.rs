b7cbb1096aea5f528a748f282dbcee7d87a89bb9
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-01-19 20:41:23 +0000

    Update lib.rs

--- rustreactapp/wasm-lib/src/lib.rs
+++ rustreactapp/wasm-lib/src/lib.rs
@@ -1,23 +1,36 @@
 use wasm_bindgen::prelude::*;
-pub fn init(){
-    let note_list = Vec::<String>::new();
+struct NoteAppRS;
+impl NoteAppRS{
+    fn addnote(mut notelist: Vec<String>,element: String){
+        notelist.push(element);
+        println!("{:?}",notelist);
+    }
+    fn delnote(mut notelist: Vec<String>,element:String){
+        let index = note_list
+        .iter()
+        .position(|&r| r == element)
+        .unwrap();
+        notelist.remove(index);
+    }
+    fn get_values(notelist: Vec<String>) -> Vec<String>{
+      notelist
+    }
 }
+fn main(){
+    let note_list = Vec::new();
+    let main = "he"; // this is an example ("he", will be whatever the user puts in as a note)
+    NoteAppRS::addnote(note_list,main.to_string());
+}
+
 #[wasm_bindgen]
-pub fn addnote(element: String){
-    note_list.push(element);
-    println!("{:?}",note_list);
+pub fn addnote(element:String){
+    NoteAppRS::addnote(note_list,element);
 }
 #[wasm_bindgen]
 pub fn delnote(element:String){
-    let index = note_list
-    .iter()
-    .position(|&r| r == element)
-    .unwrap();
-    note_list.remove(index);
+    NoteAppRs::delnote(note_list,element)
 }
 #[wasm_bindgen]
 pub fn get_values(){
-    for note in note_list.iter(){
-        return note.to_string();
-    };
+    get_values(note_list)
 }
