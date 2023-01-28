65ce7eeb45a62b643cae2e6e4d1c3f476c91e206
Clive <117390912+TheLinuxGuyy@users.noreply.github.com>
2023-01-28 18:47:48 +0000

    Update lib.rs

--- rustreactapp/wasm-lib/src/lib.rs
+++ rustreactapp/wasm-lib/src/lib.rs
@@ -7,38 +7,41 @@ struct Note{
     id: i64,
     content: String,
 }
-
-
-#[tokio::main]
-async fn main() -> Result<(), sqlx::Error>{
-    let pool = PgPoolOptions::new().max_connections(5)
-    .connect("postgres://postgres:welcom@localhost/postgres")
-    .await?;
-    sqlx::query(
-        r#"
-        CREATE TABLE IF NOT EXISTS notes(
-            id bigserial,
-            content text
-        );
-        "#,
-    ).execute(&pool)
-    .await?;
- 
-    // inserting a new note
-    let row: (i64,) = sqlx::query_as("insert into notes (content) values ($1) returning id")
-    .bind("a new note")
-    .fetch_one(&pool)
-    .await?;
+struct SqlStruct;
+impl SqlStruct{
+    #[tokio::main]
+    pub async fn main() -> Result<(), sqlx::Error>{
+        let pool = PgPoolOptions::new().max_connections(5)
+        .connect("postgres://postgres:welcom@localhost/postgres")
+        .await?;
+        sqlx::query(
+            r#"
+            CREATE TABLE IF NOT EXISTS notes(
+                id bigserial,
+                content text
+            );
+            "#,
+        ).execute(&pool)
+        .await?;
+        };
+    pub fn inserting_note(){
+        let row: (i64,) = sqlx::query_as("insert into notes (content) values ($1) returning id")
+            .bind("a new note")
+            .fetch_one(&pool)
+            .await?;
+        };
 
     //selecting all the notes
-    let rows =sqlx::query("SELECT * FROM notes").fetch_all(&pool).await?;
-    
-    // making the result string to pass to JS
-    let str_result = rows
-    .iter()
-    .map(|r| format!("{} - {}",r.get::<i64,_>("id"),r.get::<String,_>("name"))
-    .collect::<Vec<String>>()
-    .join(", ");
+    pub fn select_rows() -> str{
+        let rows =sqlx::query("SELECT * FROM notes").fetch_all(&pool).await?;
+        // making the result string to pass to JS
+        let str_result = rows
+        .iter()
+        .map(|r| format!("{} - {}",r.get::<i64,_>("id"),r.get::<String,_>("name")))
+        .collect::<Vec<String>>()
+        .join(", ");
+        str_result
+    };
 
     let select_query = sqlx::query("SELECT id, content FROM notes");
     let notes: Vec<Note> = select_query
@@ -48,7 +51,7 @@ async fn main() -> Result<(), sqlx::Error>{
     })
     .fetch_all(&pool)
     .await?;
-    
+
 
 
     Ok(())
